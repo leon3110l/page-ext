143d9b2d9b5080f3848acc343d956728530e0629
Armand_laptop <517226@edu.rocmn.nl>
2017-12-18 04:03:22 +0100

    added remove Domainshizzle to myFork

--- main.js
+++ main.js
@@ -2,70 +2,297 @@
  Program Settings
 *****************/
 const programSettings = {
-    linkColourer : {
-        methodEnabled: false,
+    linkColourer: {
+        methodEnabled: true,
 
-        red: false,
+        red: true,
         orange: false,
         yellow: false
     },
 
-    smokeTest : {
-        methodEnabled : false
+    // Test Settings
+    smokeTest: {
+        methodEnabled: true
+    },
+
+    removeOnCode: {
+        methodEnabled: true,
+        colorAffectedElements: false
+    },
+
+    arrayToString : {
+        addDot: false
+    },
+
+    urlRemover : {
+        functionEnabled: true
     }
 };
 
-/*
---------------
- linkColourer
- -------------
 
- --Colours the links based upon some specified rules
-*/
-(linkColourer = function (settings) {
-    if (settings.methodEnabled == true) {
-        const tables = document.querySelectorAll('#urlform table')
+/*-----------------------
+ Start of the singletons
+-----------------------*/
 
-        const endCharacters = /([:.?!])/g           // specifies when a thing ends
-        const remCharacters = /([|]{2})|([≥()])/g   // specifies the stuff to take out
-        const website = /([--:\w?@%&+~#=]*\.[a-z]{2,4}\/{0,2})((?:[?&](?:\w+)=(?:\w+))+|[--:\w?@%&+~#=]+)?/g // website check
+const paginaMaken = (function (programSettings) {
 
+    /*----------------
+     Private variables
+    -----------------*/
 
-        for (let table of tables) {
-            const tags = table.querySelectorAll('tbody tr td ul li');
+    const tableRows = document.querySelectorAll('#urlform table tbody tr');
+    const endCharacters = /([:.?!])/g;           // specifies when a thing ends
+    const remCharacters = /([|]{2})|([≥()])/g;   // specifies the stuff to take out
+    const website = /([--:\w?@%&+~#=]*\.[a-z]{2,4}\/{0,2})((?:[?&](?:\w+)=(?:\w+))+|[--:\w?@%&+~#=]+)?/g; // website check
 
-            for (let tag of tags) {
-                const state = {
-                    site: false,
-                }
-                const text = tag.querySelector('span').innerHTML.toLowerCase();
+    return {
 
-                if (settings.red == true) {
-                    if (text.length === 1 && text != '/') {
-                        tag.style.border = '5px solid red'
+        /*----------------
+         Public Methods
+        -----------------
+
+        ----------
+         UrlRemover
+        ------------
+
+        --Removes domain names from the page*/
+        urlRemover : (function () {
+            if (programSettings.urlRemover.functionEnabled) {
+                (runScript = function () {
+                    let id_pos = [];
+                    let value = "";
+
+                    let removeCode = "";
+                    let testArray = [];
+
+                    for (let tableRow of tableRows) {
+
+                        const tags = tableRow.querySelectorAll('td ul li');
+                        for (let tag of tags) {
+
+                            setTimeout(function () {
+                                const text = tag.querySelector('span').innerHTML;
+                                const state = { site: false}
+                                // split the string;
+                                testArray = splitter(text);
+
+                                // create a binary removeCode;
+                                removeCode = testLooper(testArray);
+
+                                // Remove elements from array based on binaryCode
+                                testArray = removeOnCode(removeCode, testArray, (tag.querySelector('span').style) );
+
+                                // convert the array back to a string
+                                value = arrayToString(testArray);
+
+                                // replaces the original dom item with the newly computed one
+                                tag.querySelector('span').innerHTML = value;
+                                tag.querySelector('input').value = value;
+
+                                const ui = document.querySelector('input.hidden').value;
+                                let id_pos = tag.id.split("_");
+                                sendAjax(ui, id_pos, value)
+
+                            }, 2000);
+                        }
+                    }
+                })();
+            }
+            function splitter(string) {
+                const splittedString = string.split(".");
+                return splittedString;
+            }
+
+            function testLooper(arrayOfStr) {
+                let results = "";
+
+                for (var i = 0; i < arrayOfStr.length; i++) {
+
+                    if ( containsDomainCode(arrayOfStr[i]) === true) {
+                        results += "1";
+                    } else if ( containsDomainCode(arrayOfStr[i]) === false ) {
+                        results += "0";
                     }
                 }
 
-                if (settings.orange == true) {
-                    if (
-                        text.search(remCharacters) > -1 ||
-                        text.search(website) > -1 ||
-                        text.indexOf('home') > -1
-                    ) {
-                        tag.style.border = '5px solid orange'
-                        state.site = true
+                return (results);
+            }
+
+            function containsDomainCode(string) {
+                const removeList = [
+                    "com",
+                    "eu",
+                    "infonu",
+                    "net",
+                    "nl",
+                    "org",
+                    "www",
+                    "be",
+                ];
+                for (var i = 0; i < removeList.length; i++) {
+                    if (string.toLowerCase() == removeList[i]) {
+                        return true;
+                    };
+                }
+                return false;
+            }
+
+            function removeOnCode(removeCode, testArray, element) {
+
+                if (programSettings.removeOnCode.methodEnabled) {
+                    const removeCodeArray = removeCode.split("");
+
+                    // console.log(i);
+                    for (let i=testArray.length-1;  i>-1;  i--) {
+                        if (removeCodeArray[i] == '1') {
+
+                            if (programSettings.removeOnCode.colorAffectedElements) {
+                                testArray.splice(i, 1);
+                                element.backgroundColor = "red"
+                            } else {
+                                testArray.splice(i, 1);
+                            }
+                        }
+                        else if (removeCodeArray[i] == '0') {
+
+                        } else {
+                            Alert("removeCodeBroke");
+                        }
                     }
                 }
+                return testArray;
+            }
 
-                if (settings.yellow == true) {
-                    if (text.search(endCharacters) > -1 && !state.site) {
-                        tag.style.border = '5px solid yellow'
+            function arrayToString(array) {
+                let string = "";
+
+                for (var i = 0; i < array.length; i++) {
+                    string += array[i];
+
+                    if (programSettings.arrayToString.addDot) {
+                        string += ".";
+                    }
+                }
+                return string;
+            }
+
+            // xhr.open('POST', 'http://51.255.87.34/~pagina/ajax/change_tag.php');
+
+            function sendAjax(ui, id_pos, value) {
+
+                const L_ui = ui;
+                const L_id = id_pos[0];
+                const L_pos = id_pos[1];
+                const L_value = value;
+
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", 'http://51.255.87.34/~pagina/ajax/change_tag.php', true);
+
+                //Send the proper header information along with the request
+                xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+                xhr.onreadystatechange = function() {//Call a function when the state changes.
+                    if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+                        // Request finished. Do processing here.
+                    }
+                }
+                xhr.send('ui=' + L_ui + "&id=" + L_id + "&pos=" + L_pos + "&value=" + L_value)
+            }
+
+        })(),
+
+
+
+        /*-----------
+         linkColourer
+        -------------
+
+        --Colours the links based upon some specified rules*/
+        linkColourer : (function (settings) {
+            if (settings.methodEnabled == true) {
+
+                for (let tableRow of tableRows) {
+                    const tags = tableRow.querySelectorAll('td ul li');
+
+                    for (let tag of tags) {
+                        const state = {
+                            site: false,
+                        }
+                        const text = tag.querySelector('span').innerHTML.toLowerCase();
+
+                        if (settings.red == true) {
+                            if (text.length === 1 && text != '/') {
+                                tag.style.border = '5px solid red'
+                            }
+                        }
+
+                        if (settings.orange == true) {
+                            if (
+                                text.search(remCharacters) > -1 ||
+                                text.search(website) > -1 ||
+                                text.indexOf('home') > -1
+                            ) {
+                                tag.style.border = '5px solid orange'
+                                state.site = true
+                            }
+                        }
+
+                        if (settings.yellow == true) {
+                            if (text.search(endCharacters) > -1 && !state.site) {
+                                tag.style.border = '5px solid yellow'
+                            }
+                        }
                     }
                 }
             }
-        }
+        })(programSettings.linkColourer)
     }
-})(programSettings.linkColourer);
+})(programSettings);
+
+// (linkColourer = function (settings) {
+//     if (settings.methodEnabled == true) {
+//         const tables = document.querySelectorAll('#urlform table')
+//
+//         const endCharacters = /([:.?!])/g           // specifies when a thing ends
+//         const remCharacters = /([|]{2})|([≥()])/g   // specifies the stuff to take out
+//         const website = /([--:\w?@%&+~#=]*\.[a-z]{2,4}\/{0,2})((?:[?&](?:\w+)=(?:\w+))+|[--:\w?@%&+~#=]+)?/g // website check
+//
+//
+//         for (let table of tables) {
+//             const tags = table.querySelectorAll('tbody tr td ul li');
+//
+//             for (let tag of tags) {
+//                 const state = {
+//                     site: false,
+//                 }
+//                 const text = tag.querySelector('span').innerHTML.toLowerCase();
+//
+//                 if (settings.red == true) {
+//                     if (text.length === 1 && text != '/') {
+//                         tag.style.border = '5px solid red'
+//                     }
+//                 }
+//
+//                 if (settings.orange == true) {
+//                     if (
+//                         text.search(remCharacters) > -1 ||
+//                         text.search(website) > -1 ||
+//                         text.indexOf('home') > -1
+//                     ) {
+//                         tag.style.border = '5px solid orange'
+//                         state.site = true
+//                     }
+//                 }
+//
+//                 if (settings.yellow == true) {
+//                     if (text.search(endCharacters) > -1 && !state.site) {
+//                         tag.style.border = '5px solid yellow'
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// })(programSettings.linkColourer);
 
 /*
   ----------------------
