ff681bf040eb0fe3874ad9ccdf4708efe69cb8d9
Armand_laptop <517226@edu.rocmn.nl>
2017-12-25 00:59:59 +0100

    restructured the Project

--- RunMe_windowsOnly.bat
+++
@@ -1,8 +0,0 @@
-echo website: http://51.255.87.34/~pagina/login.php
-echo email: Armand-van-Alphen@outlook.com
-echo pass: kbFY8SBo$
-
-start cmd /k web-ext run
-
-pause
-pause
---
+++ paginator/pagina_maken/pageInfo_Module.js
@@ -0,0 +1,8 @@
+// try {
+//     const pageInfo_Module = (function() {
+//
+//     })();
+// } catch (err) {
+//     console.log(err)
+//     alert("pageInfo broke")
+// }
